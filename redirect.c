729917cce61721ad3354000a0ab904291c029cde
habibsain <habibsain25@gmail.com>
2025-05-31 16:23:46 +0530

    using dup2 to redirect

---
+++ redirect.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+void main()
+{
+	int val = open("output.txt", O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if( val < 0)
+	{
+		perror("open);
+		exit(1);
+	}
+	dup2(val,1);
+
+	printf("This is written in /std_out but redirected to output file");
+
+}
+
