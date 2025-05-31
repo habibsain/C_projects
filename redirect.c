96092ae206f643bf25d0d04d3dbce307d99a7512
habibsain <habibsain25@gmail.com>
2025-05-31 16:27:37 +0530

    compiled redirect

--- redirect.c
+++ redirect.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<fcntl.h>
 #include<unistd.h>
 #include<stdlib.h>
 
@@ -7,7 +8,7 @@ void main()
 	int val = open("output.txt", O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if( val < 0)
 	{
-		perror("open);
+		perror("open");
 		exit(1);
 	}
 	dup2(val,1);
