556cd0a7890054283083d43d51740e1c8c6135e4
habibsain <habibsain25@gmail.com>
2025-06-03 21:53:09 +0530

    redirected stdout

--- piping.c
+++ piping.c
@@ -10,6 +10,17 @@ void main(int argc, char **argv)
 		fprintf(stderr, "Try: lsgrep arg1 arg2\n");
 		exit(1);
 	}
+	//save stdin and stdout file descrptors
+	int temp_in = dup(0);
+	int temp_out = dup(1);
+
+	//make a pipe
+	int fdpipe[2];
+	pipe(fdpipe);
+
+	//redirect stdout for first process
+	dup2(fdpipe[1], 1);
+	close(fdpipe[1]);
 
 	
 }
