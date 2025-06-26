b224d3514760fb0438685dda05d48f94721c01b4
habibsain <habibsain25@gmail.com>
2025-06-26 18:36:31 +0530

    created memory allocation program

---
+++ include/mem.h
@@ -0,0 +1,16 @@
+#ifndef __MEM__
+#define __MEM__
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAX 1
+
+void* NEW(size_t size);
+
+void* FREE(void* ptr);
+
+
+
+
+#endif
---
+++ src/mem.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "../include/mem.h"
+
+void* NEW(size_t size)
+{
+    void* ptr = malloc(size);
+
+    if(ptr == NULL)
+    {
+        frintf(stderr,"Memory allocation error");
+        exit(EXIT_FAILURE);
+    }
+    return ptr;
+}
+
+void FREE(void* ptr)
+{
+    if(ptr != NULL)
+    {
+        free(ptr);
+        ptr = NULL;
+    }
+    else
+    {
+        fprintf(stderr, "Ptr is unallocated");
+    }
+}
