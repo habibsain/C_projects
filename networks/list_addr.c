d658b02a4465c85db09f44a2a5972d7d81a34175
habibsain <habibsain25@gmail.com>
2025-06-28 14:43:48 +0530

    differnt headers for diff os

--- networks/list_addr.c
+++ networks/list_addr.c
@@ -1,8 +1,21 @@
+#if defined(_WIN32)
+
+#ifndef _WINNIT32
+#define _WINNIT32 0x0600
+#endif
+
+#include<winsock.h>
+#include<iphlptcp.h>
+#pragma comment(lib, "")
+
+#else
 #include<sys/socket.h>
 #include<netdb.h>
+#include<ifaddrs.h>
+#endif
+
 #include<stdio.h>
 #include<stdlib.h>
-#include<ifaddrs.h>
 
 int main(int argc, char* argv)
 {
