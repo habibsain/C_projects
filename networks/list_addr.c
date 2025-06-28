94b1b1f54f375f22b92dc5f6d5eaacf9096a00c5
habibsain <habibsain25@gmail.com>
2025-06-28 17:50:08 +0530

    linux part completed

--- networks/list_addr.c
+++ networks/list_addr.c
@@ -1,23 +1,29 @@
 #if defined(_WIN32)//windows
 
-#ifndef _WINNIT32
-#define _WINNIT32 0x0600
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
 #endif
 
-#include<winsock.h>
-#include<iphlptcp.h>
-#pragma comment(lib, "")
+#include<winsock2.h>
+#include<ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
 #else//Linux
+#include<sys/types.h>
 #include<sys/socket.h>
 #include<netdb.h>
 #include<ifaddrs.h>
+#include<fcntl.h>
+#include<net/if.h>
+#include<netinet/in.h>
+#include<string.h>
+#include<arpa/inet.h>
 #endif
 
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(int argc, char* argv)
+int main()
 {
     #if defined(_WIN32)//Windows
 
@@ -35,18 +41,26 @@ int main(int argc, char* argv)
     
     while (address)
     {
-        char* family = address->ifa_addr->sa_family;
+        int family = address->ifa_addr->sa_family;
         if(family == AF_INET || family == AF_INET6)
         {
             printf("%s\t", address->ifa_name);
             printf("%s\n", family == AF_INET ? "IPv4" : "IPv6");
-        }
+        
 
         char ap[100];
-
-        address->ifa_next;
+        const int family_size = family == AF_INET ? \
+            sizeof(struct sockaddr_in) : sizeof(struct sockaddr_in6);
+        
+        //getnameinfo(address->ifa_addr, family_size ,ap, sizeof(ap), 0, 0, NI_NUMERICHOST);
+        //printf("\t%s\n", ap);
+        }
+        
+    address->ifa_next;
     }
-    
+
+    freeifaddrs(addresses);
+    return 0;
 
     #endif
 }
