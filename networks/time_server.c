8376b4805f3cb6ab1ae323ee5faa15cadfff9a3b
habibsain <habibsain25@gmail.com>
2025-06-30 03:20:55 +0530

    added accepting connection and getting the client addr

--- networks/time_server.c
+++ networks/time_server.c
@@ -55,6 +55,7 @@ SOCKET create_socket(struct addrinfo* address_ptr)
     return new_socket;
 }
 
+
 int main()
 {
     #if defined(_WIN32)//windows
@@ -109,6 +110,28 @@ int main()
         fprintf(stderr, "listen() failed. (%d)\n", GETSOCKETERRORNO());
         return 1;
     }
+
+    //accept incomming connection
+    printf("Waiting for  connection----\n");
+
+    struct sockaddr_storage client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    
+    SOCKET socket_client = accept(socket_listen, (struct sockaddr*) &client_addr, &client_len);
+
+    if(!ISVALIDSOCKET(socket_client))
+    {
+        fprintf(stderr, "accept() failed. (%d)\n", GETSOCKETERRORNO());
+        return 1;
+    }
+
+    //print client's address
+    printf("Client is connected-----\n");
+    
+    char address_buffer[100];
+    getnameinfo((struct sockaddr*) &socket_client, client_len, address_buffer, sizeof(address_buffer), 0, 0, NI_NUMERICHOST);
+
+    printf("%s\n", address_buffer);
 
     return 0;
 }
