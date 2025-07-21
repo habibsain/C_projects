96321180c9998b28602eafff463284e8a7148be2
habibsain <habibsain25@gmail.com>
2025-07-22 02:54:12 +0530

    initial code for spare

--- Bowling/bowling_game.c
+++ Bowling/bowling_game.c
@@ -2,19 +2,42 @@
 
 #include "bowling_game.h"
 
-static int score;
+#define MAX_ROLLS 21
+
+int rolls[MAX_ROLLS];//stores each try pin number
+int current_roll;//flag 
 
 void bowling_game_init()
 {
-	score = 0;
+	for (int i = 0; i < MAX_ROLLS; i++)
+		rolls[i] = 0;
+	
+	current_roll = 0;
 }
 
 void bowling_game_roll(int pins)
 {
-	score += pins;
+	rolls[current_roll] = pins;
+	current_roll++; 
 }
 
 int bowling_game_score()
 {
+	int score = 0;
+	
+	//sum over all
+	for (int i = 0; i < MAX_ROLLS; i++)
+	{
+		score += rolls[i];
+		//check for spare
+		if (i%2 == 0 && (rolls[i] + rolls[i+1]) == 10 )
+		{
+			if (i + 2 < MAX_ROLLS)
+			{
+				score += rolls[i+2];
+			}
+		}
+	}
+
 	return score;
 }
